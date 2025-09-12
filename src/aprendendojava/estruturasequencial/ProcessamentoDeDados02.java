6158a50d828215dd61717632b8874eec1a016355
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2025-09-12 16:09:38 -0300

    Aprendizado sobre atribuição e casting Prática com o operador de atribuição (=) para tipos primitivos. Demonstração do casting explícito (narrowing conversion), por exemplo,   de 'double' para 'int', e a consequente perda de precisão (truncamento). Exploração do casting implícito (widening conversion), onde um tipo   menor é automaticamente promovido para um tipo maior (ex: 'int' para 'double').

---
+++ src/aprendendojava/estruturasequencial/ProcessamentoDeDados02.java
@@ -0,0 +1,14 @@
+package aprendendojava.estruturasequencial;
+
+public class ProcessamentoDeDados02 {
+    public static void main(String[] args) {
+        int x;
+        double y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
---
+++ src/aprendendojava/estruturasequencial/ProcessamentoDeDados04.java
@@ -0,0 +1,14 @@
+package aprendendojava.estruturasequencial;
+
+public class ProcessamentoDeDados04 {
+    public static void main(String[] args) {
+
+        int a, b;
+        double result;
+
+        a = 5;
+        b = 2;
+        result = (double) a / b; //Casting -> Conversão explícita dos valores
+        System.out.println(result);
+    }
+}
