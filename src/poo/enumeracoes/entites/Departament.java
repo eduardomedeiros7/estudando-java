bd1c578244db7f9f8e7df4f00504cad85ec02bbc
Eduardo Medeiros <eduardomedeiroseng7@gmail.com>
2026-04-05 09:24:47 -0300

    implement worker salary system using object composition
    - Created Worker, Department, and HourContract entities.
    - Implemented Enum for WorkerLevel (Junior, Mid_Level, Senior).
    - Applied Object Composition and Association between Worker and Department.
    - Implemented income calculation by delegating total value logic to HourContract.

---
+++ src/poo/enumeracoes/entites/Departament.java
@@ -0,0 +1,18 @@
+package poo.enumeracoes.entites;
+
+public class Departament {
+
+    private String name;
+
+    public Departament(){
+
+    }
+    public Departament(String name){
+        this.name = name;
+    }
+
+
+    public  void setName(String name){
+        this.name = name;
+    }
+}
